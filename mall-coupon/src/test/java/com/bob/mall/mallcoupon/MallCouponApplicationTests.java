3e73921b8d62759ac99c30615e11f0ef7715a7c1
Easy-Bob <2428059449@qq.com>
2025-09-25 16:27:51 -0500

    1. project initialization

---
+++ mall-coupon/src/test/java/com/bob/mall/mallcoupon/MallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bob.mall.mallcoupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
